00753902af5f426620acd36e8b6a6c28f1931b83
philpill <github@philpill.net>
2014-09-20 20:31:32 +0100

    update sprite

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>impact-js</title>
+    <title>That Bloody Game</title>
     <link rel="stylesheet" href="/static/css/main.css" />
   </head>
   <body>
     <canvas id="canvas"></canvas>
 
-    <script src="/static/js/built.js" type="text/javascript"></script>
-
 <?php
 /*
-    <script src="/lib/impact/impact.js" type="text/javascript"></script>
-    <script src="/lib/game/main.js" type="text/javascript"></script-->
+    <script src="/static/js/built.js" type="text/javascript"></script>
 */
 ?>
-
+    <script src="/lib/impact/impact.js" type="text/javascript"></script>
+    <script src="/lib/game/main.js" type="text/javascript"></script>
   </body>
 </html>
