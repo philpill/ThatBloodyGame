a849ce422ccfd5aaf5b0b6ed25f6a654bec2c1fc
philpill <github@philpill.net>
2014-09-21 00:30:59 +0100

    sprite

--- index.php
+++ index.php
@@ -5,14 +5,17 @@
     <link rel="stylesheet" href="/static/css/main.css" />
   </head>
   <body>
+
+    <h1><a href="http://thatbloodygame.co.uk">That Bloody Game</a></h1>
+
     <canvas id="canvas"></canvas>
 
+    <script src="/static/js/built.js" type="text/javascript"></script>
 <?php
 /*
-    <script src="/static/js/built.js" type="text/javascript"></script>
-*/
-?>
     <script src="/lib/impact/impact.js" type="text/javascript"></script>
     <script src="/lib/game/main.js" type="text/javascript"></script>
+*/
+?>
   </body>
 </html>
