b17c4f39ffd3074981b0312c406ae5f8613b4fe6
philpill <github@philpill.net>
2014-09-21 20:21:48 +0100

    badrock

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
 
     <canvas id="canvas"></canvas>
 
-    <script src="/static/js/built.js" type="text/javascript"></script>
 <?php
 /*
-    <script src="/lib/impact/impact.js" type="text/javascript"></script>
-    <script src="/lib/game/main.js" type="text/javascript"></script>
+    <script src="/static/js/built.js" type="text/javascript"></script>
 */
 ?>
+    <script src="/lib/impact/impact.js" type="text/javascript"></script>
+    <script src="/lib/game/main.js" type="text/javascript"></script>
   </body>
 </html>
